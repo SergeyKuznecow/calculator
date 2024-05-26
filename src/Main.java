ddd5a658c6262b0c683f6fb9b2457e24fb2352df
SergeyKuznecow <GFSN@rambler.ru>
2024-05-27 02:22:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Input:");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String input = scanner.nextLine();
+            String[] symbols = input.split(" ");
+            if (symbols.length != 3) {
+                throw new IllegalArgumentException("throws Exception // вводите числа и символ операции через пробел и не более 2-х.");
+            }
+            int a = Integer.parseInt(symbols[0]);
+            int b = Integer.parseInt(symbols[2]);
+            if (a > 10 || b > 10 ) {
+                throw new IllegalArgumentException("По условию задачи вводите числа не более 10.");
+            }
+            int result = switch (symbols[1]) {
+                case "+" -> a + b;
+                case "-" -> a - b;
+                case "*" -> a * b;
+                case "/" -> {
+                    if (b == 0) {
+                        throw new ArithmeticException("Деление на 0 недопустимо!");
+                    }
+                    yield a / b;
+                }
+                default ->
+                        throw new IllegalArgumentException("Недопустимая операция. Выбирайте следующие символы: +, -, *, /");
+            };
+            System.out.println("Output:");
+            System.out.println(result);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+
+
+
+
         }
     }
 }
